4e88a136a946584ff82556f3345fa504ba46bb61
drowkid-1 <pxne031112hmcrgda1@bggem.mx>
2023-09-12 22:46:11 -0500

    headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Inicio' },
   { href: '/blog', title: 'Posts' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Proyectos' },
+  { href: '/projects', title: 'Destacados' },
   { href: '/about', title: 'About' },
 ]
 
