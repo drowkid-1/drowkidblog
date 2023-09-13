7a29c39a5a3ecbe4f9625b9c604735cc5d9f3d5a
drowkid-1 <pxne031112hmcrgda1@bggem.mx>
2023-09-12 22:53:55 -0500

    dk

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,13 +2,13 @@ const projectsData = [
   {
     title: 'Servidores VPS Premium',
     description: `Servidores VPS Premium actualizados (01-SEP-2023)`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/vps.png',
     href: '/blog/vpspremium',
   },
   {
     title: '¡Descarga mi app!',
     description: `¡Ya está disponible mi app "Drowk-App" donde encontrarás Herramientas, Servidores VPS, cursos, scripts, apps y muchas cosas más ANTES QUE TODOS!`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/drowklogo.png',
     href: '/blog/drowkapp',
   },
 ]
